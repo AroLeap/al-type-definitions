8877872e0061981d1ed67d0f8a5f9ca4cee1c5f1
aditya-aroleap <aditya.p@aroleap.com>
2022-04-08 13:33:09 +0530

    Added type exports.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-export {} from './typeDatabase/Coaches/types' 
-export {} from './typeDatabase/ExerciseDatabase/types'
-export {} from './typeDatabase/PlanDatabase/types' 
+export {CoachIdsType,CoachType} from './typeDatabase/Coaches/types' 
+export {MuscleGroupIDs,MainMuscleGroupsType,MainMuscleGroupsDictDataType,MainMuscleGroupsDictType,DeepMuscleGroupsType,DeepMuscleGroupDictType} from './typeDatabase/ExerciseDatabase/types'
+export {DailySmartWorkoutInfoType,PlanInfoType} from './typeDatabase/PlanDatabase/types' 
 export const Greeter = (name: string) => `Hello ${name}`; 
 // export {CoachType,CoachIdsType}
