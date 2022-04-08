811a5193e4dfd925f8dcfb5161318e0353f1f833
aditya-aroleap <aditya.p@aroleap.com>
2022-04-08 13:20:42 +0530

    Added new exports.

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,5 @@
+export {} from './typeDatabase/Coaches/types' 
+export {} from './typeDatabase/ExerciseDatabase/types'
+export {} from './typeDatabase/PlanDatabase/types' 
 export const Greeter = (name: string) => `Hello ${name}`; 
+// export {CoachType,CoachIdsType}
