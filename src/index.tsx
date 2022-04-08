ae0fa6c45501d936b632620a3d542cf71a333879
aditya-aroleap <aditya.p@aroleap.com>
2022-04-08 13:36:58 +0530

    Removed unwanted.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
 export {CoachIdsType,CoachType} from './typeDatabase/Coaches/types' 
 export {MuscleGroupIDs,MainMuscleGroupsType,MainMuscleGroupsDictDataType,MainMuscleGroupsDictType,DeepMuscleGroupsType,DeepMuscleGroupDictType} from './typeDatabase/ExerciseDatabase/types'
 export {DailySmartWorkoutInfoType,PlanInfoType} from './typeDatabase/PlanDatabase/types' 
-export const Greeter = (name: string) => `Hello ${name}`; 
-// export {CoachType,CoachIdsType}
